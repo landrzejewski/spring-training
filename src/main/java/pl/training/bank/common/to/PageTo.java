c554f336411df56f18a4a24dff6f542c9d66ff77
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-12-20 10:22:15 +0100

    Validator

--- src/main/java/pl/training/bank/common/to/PageTo.java
+++ src/main/java/pl/training/bank/common/to/PageTo.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
-@XmlRootElement(name = "page")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
