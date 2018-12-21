4e5b2939a17cbb933e30c1df4675b5a42d29a4d4
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-12-21 09:56:34 +0100

    Validator

--- src/main/java/pl/training/bank/MvcConfig.java
+++ src/main/java/pl/training/bank/MvcConfig.java
@@ -1,13 +1,8 @@
 package pl.training.bank;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.training.bank.common.mapper.Mapper;
@@ -18,20 +13,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
-@ComponentScan("pl.training.bank")
 @EnableSwagger2
-@EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
-
-    @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("exceptions");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
 
     @Bean
     public Mapper mapper() {
